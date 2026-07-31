a5eeee767f9b804483c88ddfcb6c971a54d6d677
sappatitrivi-star <sappatitrivi@gmail.com>
2026-07-31 11:26:25 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,25 +1,25 @@
-
-class Employee {
-
-    private int salary;
-
-    public void setSalary(int s) {
-        salary = s;
-    }
-
-    public int getSalary() {
-        return salary;
+class NumA{
+    int valueA;
+    public NumA(int valueA){
+        this.valueA=valueA;
     }
 }
+class NumB extends NumA{
+    int valueB;
+     public NumB(int valueA,int valueB){
+        super(valueA);
+        this.valueB=valueB;
+     }
+}
+
 
 public class Main {
+    public static void main(String[] args) {
+        NumB numbers=new NumB(50,30);
+        int sum=numbers.valueA+numbers.valueB;
+        System.out.println("First number from (super class):"+numbers.valueA);
+                System.out.println("First number from (sub class):"+numbers.valueB);
+                        System.out.println("Total Sum:"+sum);
 
-    public static void main(String args[]) {
-
-        Employee e = new Employee();
-
-        e.setSalary(50000);
-
-        System.out.println(e.getSalary());
     }
 }
