addefd9a80e970b4895344be35bba53281b787db
sappatitrivi-star <sappatitrivi@gmail.com>
2026-08-13 18:51:48 +0530

    Add files via upload

---
+++ topic wise concept/Main1.java
@@ -0,0 +1,25 @@
+class NumA{
+    int valueA;
+    public NumA(int valueA){
+        this.valueA=valueA;
+    }
+}
+class NumB extends NumA{
+    int valueB;
+     public NumB(int valueA,int valueB){
+        super(valueA);
+        this.valueB=valueB;
+     }
+}
+
+
+public class Main1 {
+    public static void main(String[] args) {
+        NumB numbers=new NumB(50,30);
+        int sum=numbers.valueA+numbers.valueB;
+        System.out.println("First number from (super class):"+numbers.valueA);
+                System.out.println("First number from (sub class):"+numbers.valueB);
+                        System.out.println("Total Sum:"+sum);
+
+    }
+}
