a3f456e77cc181682c8bc7a864a12dcf845acb8e
sappatitrivi-star <sappatitrivi@gmail.com>
2026-08-13 19:00:03 +0530

    Add files via upload

---
+++ topic wise concept/Selection statemnts/Foreach.java
@@ -0,0 +1,9 @@
+public class Foreach{
+    public static void main(String[] args) {
+        String[] fruits={"apple","orange","mango"};
+        for(String fruit : fruits){
+        System.out.println("Fruit:"+fruit);
+    }
+    }
+
+    }
