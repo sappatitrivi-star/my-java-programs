e93d4be59670da0d884dae7b79ae96c4b14a8c36
sappatitrivi-star <sappatitrivi@gmail.com>
2026-08-29 21:00:05 +0530

    Delete topic wise concept directory

--- topic wise concept/Selection statemnts/Foreach.java
+++
@@ -1,9 +0,0 @@
-public class Foreach{
-    public static void main(String[] args) {
-        String[] fruits={"apple","orange","mango"};
-        for(String fruit : fruits){
-        System.out.println("Fruit:"+fruit);
-    }
-    }
-
-    }
